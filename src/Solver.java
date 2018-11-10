5a1c5bf8ba94f6bc21e62320116d95f0d9c17fcb
Nareg Megan <nareg@Naregs-MacBook-Pro.local>
2018-11-10 10:44:03 -0800

    add multiple arg functionality

--- src/Solver.java
+++ src/Solver.java
@@ -1,4 +1,5 @@
 import java.util.PriorityQueue;
+import java.io.*;
 
 /**
  *
@@ -7,24 +8,31 @@ import java.util.PriorityQueue;
  */
 public class Solver {
 
-  /**
-  * Return a Board object from a configuration file
-  *
-  * @param file
-  *     init config file
-  * @return
-  *     Board config board representation
-  */
-  public parseInput(File file) { //TODO: import File class and implement
+  // /**
+  // * Return a Board object from a configuration file
+  // *
+  // * @param file
+  // *     init config file
+  // * @return
+  // *     Board config board representation
+  // */
+  // public parseInput(File file) { //TODO: import File class and implement
+  //
+  // }
 
+  public boolean goalReached(Node node, int[] goalRepr) {
+    
   }
 
-
-
-
-
-  public static void main(String[] args) {
-    System.out.println("Hello World");
+  public static void main(String[] args) throws IOException{
+    if(args.length < 2 || args.length > 3) {
+      throw new IOException("Invalid number of arguments.");
+    }
+    if(args[0].substring(0, 2).equals("-o")) {
+      System.out.println("Debug");
+    } else {
+      System.out.println(args[0].substring(0, 2));
+    }
   }
 
 }
